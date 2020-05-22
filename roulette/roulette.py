e3713330b09b01d08aec682bd92854c1d24791da
Ross Warren <peregrine.warren@physics.ox.ac.uk>
2020-05-22 14:56:07 +0100

    add confidence interval, add Eu and Am wheels

--- roulette/roulette.py
+++ roulette/roulette.py
@@ -1,12 +1,11 @@
 """
-Monte Carlo simulation for calculating pi.
+Monte Carlo simulation for simulating a roulette wheel
 Author: Ross <ross dot warren at pm dot me>
 Reproduced from MIT online course 'Introduction to Computational Thinking and Data Science'
 """
 
 
 import random
-import math
 
 class Roulette():
     """Set up the roulette wheel."""
@@ -29,6 +28,26 @@ class Roulette():
     def __str__(self):
         return 'Fair Roulette Wheel'
 
+
+class EuRoulette(Roulette):
+    """European roulette wheel with one green zero. Odds do not change"""
+    def __init__(self):
+        Roulette.__init__(self)
+        self.pockets.append('0')    # add a zero pocket
+
+    def __str__(self):
+        return 'European Roulette'
+
+class AmRoulette(EuRoulette):
+    """American roulette wheel with two greens. Odds do not change"""
+    def __init__(self):
+        EuRoulette.__init__(self)
+        self.pockets.append('00')    # add a zero pocket
+
+    def __str__(self):
+        return 'American Roulette'
+
+
 def playRoulette(game, numSpins, pocket, bet):
     """Method for playing the game."""
     totPocket = 0   # initially we have won 0
@@ -36,12 +55,34 @@ def playRoulette(game, numSpins, pocket, bet):
         game.spin()
         totPocket += game.betPocket(pocket, bet)
 
-    print(numSpins, 'spins of', game)
-    print('Expected return betting', pocket, '=', str(100*totPocket/numSpins), '%\n')
+    # print(numSpins, 'spins of', game)
+    # print('Expected return betting', pocket, '=', str(100*totPocket/numSpins), '%\n')
     return (totPocket/numSpins)
 
+
+def getMeanAndStd(X):
+    """Takes a list and return mean and std."""
+    mean = sum(X)/len(X)
+    tot = 0.0
+    for x in X:
+        tot += (x - mean)**2
+    std = (tot / len(X))**0.5
+    return mean, std
+
+
+### PROGRAM ###
+
 if __name__ == '__main__':
-    game = Roulette()
-    for numSpins in (100, 100000):
-        for i in range(3):
-            playRoulette(game, numSpins, 2, 1)
+    games = (Roulette, EuRoulette, AmRoulette)
+    pocket = 2
+    numTrial = 20
+    for numSpins in (1000, 100000):
+        for game in games:
+            results = []
+            print('Simulating of', game(), ': Betting on a pocket for', numTrial, 'number of trials of ', numSpins, 'spins each')
+            for i in range(numTrial):
+                pocketReturns = playRoulette(game(), numSpins, pocket, 1)
+                results.append(pocketReturns)
+
+            mean,std = getMeanAndStd(results)
+            print('Expected return betting on', pocket, '=', round(mean*100, 2), '+/-', round(std*1.96*100, 2),  '% with 95% confidence.\n')
